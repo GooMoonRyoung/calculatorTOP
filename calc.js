854d54c3f86acec3ddab7f8f901cfd814825039c
GooMoonRyoung <Andrewtruong25@gmail.com>
2023-06-11 13:58:35 -0400

    created basic HTML of page linking to JS file and testing for basic functions

---
+++ calc.js
@@ -0,0 +1,23 @@
+let x = 12
+let y = 15
+
+function add(num1, num2){
+    console.log((num1 / num2))
+}
+
+function subtract(num1, num2){
+    console.log((num1 / num2))
+}
+
+function multiply(num1, num2){
+    console.log((num1 / num2))
+}
+
+function divide(num1, num2){
+    console.log((num1 / num2))
+}
+
+add(x, y)
+subtract(x, y)
+multiply(x, y)
+divide(x, y)
