8e5a42725e834cc901a5be682a57422827758acf
GooMoonRyoung <Andrewtruong25@gmail.com>
2023-06-11 14:15:33 -0400

    fixed the text for the JS functions

--- calc.js
+++ calc.js
@@ -2,15 +2,15 @@ let x = 12
 let y = 15
 
 function add(num1, num2){
-    console.log((num1 / num2))
+    console.log((num1 + num2))
 }
 
 function subtract(num1, num2){
-    console.log((num1 / num2))
+    console.log((num1 - num2))
 }
 
 function multiply(num1, num2){
-    console.log((num1 / num2))
+    console.log((num1 * num2))
 }
 
 function divide(num1, num2){
