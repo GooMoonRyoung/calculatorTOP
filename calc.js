deb0db03780d10c379052be59942a3007bcfa186
GooMoonRyoung <Andrew.truong25@gmail.com>
2023-08-01 01:02:39 -0400

    Added click events to all of the calculator buttons and added classes to identify each of the buttons

--- calc.js
+++ calc.js
@@ -1,6 +1,13 @@
+//constants used for testing will be changed later/removed
 let num1 = 12
 let operator = '/'
 let num2 = 15
+
+//constants that will be applied to the page
+const operands = document.querySelectorAll('.operand')
+const operators = document.querySelectorAll('.operator')
+const zero = document.querySelectorAll('.zero')
+const clear = document.querySelectorAll('.clear')
 
 function add(num1, num2){
     console.log((num1 + num2))
@@ -32,3 +39,22 @@ function operation(num1, operator, num2){
     }
 }
 operation(num1, operator, num2)
+//adding events to all of my buttons
+//Detects if an operand was clicked
+operands.forEach((operand) => {
+    operand.addEventListener('click', function(e){
+        console.log('your mom');
+    })
+})
+//Detects if an operator was clicked 
+operators.forEach((operator) => {
+    operator.addEventListener('click', function(e){
+        console.log('your dad');
+    })
+})
+zero.addEventListener('click', function(e){
+    console.log('your sister');
+})
+clear.addEventListener('click', function(e){
+    console.log('your brother');
+})
