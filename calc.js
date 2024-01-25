67ad7031b1f0a8cc62e7b203ac0e787e6fc5f3c0
ATGitFlow <andrew@everflow.io>
2024-01-25 15:11:29 -0800

    Mostly Completed calculator

--- calc.js
+++ calc.js
@@ -2,6 +2,8 @@
 let num1 
 let condition
 let num2 
+let newVal
+let hasOperator
 
 //constants that will be applied to the page
 const operands = document.querySelectorAll('.operand')
@@ -11,23 +13,39 @@ const zero = document.querySelector('.zero')
 const clear = document.querySelector('.clear')
 const display = document.getElementById('display')
 
-let displayVal = display.innerHTML
+function formatNumber(number) {
+    // Check if the number is a whole number
+    if (number % 1 === 0) {
+        return number; // Return the number as is if it's a whole number
+    } else if (number.toPrecision(3).length < 10) {
+        // If the number is fractional, format it to the specified number of decimal places
+        return number.toPrecision(3);
+    } else {
+        console.log("Your value is too fucking long")
+    }
+}
+
 
 function add(num1, num2){
-    console.log((num1 + num2))
+    newVal = Number(num1) + Number(num2)
+    display.innerHTML = formatNumber(newVal)
 }
 
 function subtract(num1, num2){
-    console.log((num1 - num2))
+    newVal = Number(num1) - Number(num2)
+    display.innerHTML = formatNumber(newVal)
 }
 
 function multiply(num1, num2){
-    console.log((num1 * num2))
+    newVal = Number(num1) * Number(num2)
+    display.innerHTML = formatNumber(newVal)
 }
 
 function divide(num1, num2){
-    console.log((num1 / num2))
+    newVal = Number(num1) / Number(num2)
+    display.innerHTML = formatNumber(newVal)
 }
+
 
 function operation(num1, operator, num2){
     if (operator === "+"){
@@ -48,49 +66,71 @@ function operation(num1, operator, num2){
 //10 characters in the display
 operands.forEach((operand) => {
     operand.addEventListener('click', function(e){
+        if (hasOperator === true){
+            display.innerHTML = ""
+            hasOperator = false
+        }
+
         if (display.innerHTML.length < 10){
-            displayVal += operand.innerHTML
-            display.innerHTML = displayVal
+            display.innerHTML += operand.innerHTML
         }
     })
 })
 //Detects if an operator was clicked 
 operators.forEach((operator) => {
     operator.addEventListener('click', function(e){
-        console.log(operator.innerHTML)
         //If this is the first number it clears the display puts the value to memory 
         if (num1 == null){
-            num1 = Number(displayVal);
+            num1 = Number(display.innerHTML);
             condition = operator.innerHTML
-            console.log(condition)
             clearDisplay()
-        }
+        } else if (num1 != null &&
+                    display.innerHTML != ""){
+            num2 = Number(display.innerHTML);
+            operation(num1, condition, num2);
+            condition = operator.innerHTML
+            num1 = Number(display.innerHTML)
+            num2 = null
+            hasOperator = true
+        } else if (num1 != null &&
+            display.innerHTML === ""){
+                alert("You are going to want to enter a 2nd number");
+            }
     })
 })
 zero.addEventListener('click', function(e){
-    console.log('your sister');
+    if (display.innerHTML.length < 10){
+        display.innerHTML += zero.innerHTML
+    }
 })
 equals.addEventListener('click', function(e){
-    num2 = displayVal
+    if (!num1){
+        return
+    }
+    if (!condition){
+        return
+    }
+    if (display.innerHTML === "0"){
+        alert("You know this isn't right come on")
+        return
+    }
+    num2 = display.innerHTML
     operation(num1, condition, num2);
+    num1 = null
+    num2 = null
 })
 clear.addEventListener('click', function(e){
-    clearDisplay();
+    clearDisplayButton();
 })
-
-function operatorConvertor(x) {
-    if (x = '+'){
-        return 1
-    }else if (x = '-'){
-        return 2
-    }else if (x = 'x'){
-        return 3
-    }else if (x = '/'){
-        return 4
-    }
-}
 
 function clearDisplay() {
     display.innerHTML = ""
-    displayVal = ""
+}
+
+function clearDisplayButton() {
+    display.innerHTML = ""
+    num1 = null
+    num2 = null
+    condition = null
+    newVal = null
 }
